e76d13ccf4a12532111167f58373d598dfa31004
Jia Long Loh <jialong.loh@grabtaxi.com>
2022-04-06 15:49:51 +0800

    Changes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use csv::Writer;
 use futures::future::try_join_all;
-use k8s_openapi::api::apps::v1::{Deployment, StatefulSet};
+use k8s_openapi::api::apps::v1::{DaemonSet, Deployment, StatefulSet};
 use k8s_openapi::api::core::v1::{Namespace, PodSpec};
 use kube::{
     api::{Api, ListParams, ObjectList},
     Client,
 };
+use serde::{Deserialize, Serialize};
 use tabled::{Table, Tabled};
+mod utils;
 
 #[derive(Debug, Clone)]
 struct InputObject {
@@ -13,17 +16,21 @@ struct InputObject {
     namespace: String,
     type_of: String,
     pod_spec: PodSpec,
+    replicas: i32,
 }
 
-#[derive(Debug, Tabled)]
+#[derive(Debug, Tabled, Clone, Serialize)]
 struct TaggedObject {
     name: String,
     namespace: String,
     type_of: String,
     container_name: String,
+    node_selectors: String,
     node_selector_check: bool,
     qos_check: bool,
     image_check: bool,
+    total_cores: f32,
+    replica_num: i32,
 }
 
 #[tokio::main]
@@ -43,12 +50,15 @@ async fn main() -> () {
 
     let mut waiters_deployments = Vec::new();
     let mut waiters_statefulsets = Vec::new();
+    let mut waiters_daemonsets = Vec::new();
 
     for namespace in namespace_list {
         let handle_deployments = tokio::spawn(get_deployments(client.clone(), namespace.clone()));
         let handle_statefulsets = tokio::spawn(get_statefulsets(client.clone(), namespace.clone()));
+        let handle_daemonsets = tokio::spawn(get_daemonsets(client.clone(), namespace.clone()));
         waiters_deployments.push(handle_deployments);
         waiters_statefulsets.push(handle_statefulsets);
+        waiters_daemonsets.push(handle_daemonsets);
     }
 
     let results_deployments: Vec<InputObject> = try_join_all(waiters_deployments)
@@ -58,6 +68,16 @@ async fn main() -> () {
         .flatten()
         .map(|x| x.clone())
         .map(|deployment| {
+            let replicas = match deployment
+                .clone()
+                .status
+                .expect("Expected statefulset status")
+                .replicas
+            {
+                Some(replica_num) => replica_num,
+                _ => 0,
+            };
+            // .expect("Expected replica count");
             let pod_spec = deployment
                 .clone()
                 .spec
@@ -80,6 +100,7 @@ async fn main() -> () {
                 namespace,
                 type_of: "deployment".to_string(),
                 pod_spec,
+                replicas,
             }
         })
         .collect();
@@ -91,6 +112,11 @@ async fn main() -> () {
         .flatten()
         .map(|x| x.clone())
         .map(|statefulset| {
+            let replicas = statefulset
+                .clone()
+                .status
+                .expect("Expected statefulset status")
+                .replicas;
             let pod_spec = statefulset
                 .clone()
                 .spec
@@ -113,6 +139,7 @@ async fn main() -> () {
                 namespace,
                 type_of: "statefulset".to_string(),
                 pod_spec,
+                replicas,
             }
         })
         .collect();
@@ -120,15 +147,37 @@ async fn main() -> () {
     let mut full_results = results_deployments.clone();
     full_results.append(&mut results_statefulsets.clone());
 
-    let tagged: Vec<TaggedObject> = full_results
+    let mut tagged: Vec<TaggedObject> = full_results
         .into_iter()
-        .map(|input| tag_object(input.name, input.namespace, input.type_of, &input.pod_spec))
+        .map(|input| {
+            tag_object(
+                input.name,
+                input.namespace,
+                input.type_of,
+                &input.pod_spec,
+                input.replicas,
+            )
+        })
         .flatten()
+        .collect();
+    tagged.sort_by(|a, b| b.total_cores.partial_cmp(&a.total_cores).unwrap());
+
+    let filtered: Vec<TaggedObject> = tagged
+        .clone()
+        .into_iter()
         .filter(|x| !x.image_check || !x.node_selector_check || !x.qos_check)
         .collect();
 
-    let table = Table::new(tagged).to_string();
+    // Sort by resources without filtering
+    let table = Table::new(&tagged).to_string();
+    // let table = Table::new(filtered).to_string();
     println!("{}", table);
+
+    // Write to csv?
+    let mut wtr = Writer::from_path("foo.csv").expect("expected valid csv writer");
+    for i in &tagged {
+        wtr.serialize(i).expect("Able to write row")
+    }
 }
 
 async fn get_deployments(client: Client, namespace: String) -> ObjectList<Deployment> {
@@ -152,16 +201,39 @@ async fn get_statefulsets(client: Client, namespace: String) -> ObjectList<State
     return deployment_list;
 }
 
+async fn get_daemonsets(client: Client, namespace: String) -> ObjectList<DaemonSet> {
+    let lp = ListParams::default();
+    let daemonsets: Api<DaemonSet> = Api::namespaced(client, &namespace);
+    let daemonset_list = daemonsets
+        .list(&lp)
+        .await
+        .expect("Expected results for daemonset list");
+    return daemonset_list;
+}
+
+fn convert_quantity_to_int(quantity: String) -> f32 {
+    match quantity {
+        x if x.contains("m") => x.replace("m", "").parse::<f32>().unwrap(),
+        x if x.contains("g") => x.replace("g", "").parse::<f32>().unwrap() * 1000.0,
+        _ => quantity.parse::<f32>().unwrap() * 1000.0,
+    }
+}
+
 fn tag_object(
     name: String,
     namespace: String,
     type_of: String,
     pod_spec: &PodSpec,
+    replicas: i32,
 ) -> Vec<TaggedObject> {
     // qos class BestEffort?
     // let bad_qos_class = pod_spec.clone().
     // dockerhub image
     let node_selector_check = pod_spec.clone().node_selector.is_some();
+    let node_selectors = match pod_spec.clone().node_selector {
+        Some(node_selector) => format!("{:?}", node_selector),
+        _ => "None".to_string(),
+    };
     let containers = pod_spec.clone().containers;
     let results: Vec<TaggedObject> = containers
         .into_iter()
@@ -169,18 +241,33 @@ fn tag_object(
             let container_name = container.name;
             let image = container.image.unwrap();
             let image_check = is_ecr_image(image);
-            let qos_check = match container.resources {
+            let qos_check = match &container.resources {
                 Some(resource) => resource.requests.is_some(),
                 None => false,
+            };
+            let cpu_request = match container.resources {
+                Some(resource) if resource.requests.is_some() => convert_quantity_to_int(
+                    resource
+                        .requests
+                        .expect("Expected requests")
+                        .get("cpu")
+                        .unwrap()
+                        .clone()
+                        .0,
+                ),
+                _ => 0.0,
             };
             TaggedObject {
                 name: name.clone(),
                 namespace: namespace.clone(),
                 type_of: type_of.clone(),
                 container_name,
+                node_selectors: node_selectors.clone(),
                 node_selector_check,
                 qos_check,
                 image_check,
+                total_cores: cpu_request * replicas as f32 / 1000.0,
+                replica_num: replicas,
             }
         })
         .collect();
@@ -191,7 +278,6 @@ fn is_ecr_image(image: String) -> bool {
     return image.contains("amazonaws.com/");
 }
 
-// 095116963143.dkr.ecr.ap-southeast-1.amazonaws.com/datadog-agent:7.32.4
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -199,6 +285,7 @@ mod tests {
     // use k8s_openapi::apimachinery::pkg::api::resource::quantity;
     use k8s_openapi::apimachinery::pkg::api::resource::Quantity;
     use std::collections::BTreeMap;
+    use utils;
 
     #[test]
     fn test_is_ecr_image() {
@@ -216,69 +303,20 @@ mod tests {
         let mut resource_requests = BTreeMap::new();
         resource_requests.insert("cpu".to_string(), Quantity("400m".to_string()));
         let pod_spec = PodSpec {
-            active_deadline_seconds: None,
-            affinity: None,
-            automount_service_account_token: None,
             containers: vec![Container {
-                args: None,
-                command: None,
-                env: None,
-                env_from: None,
                 image: Some(
                     "095116963143.dkr.ecr.ap-southeast-1.amazonaws.com/datadog-agent:7.32.4"
                         .to_string(),
                 ),
-                image_pull_policy: None,
-                lifecycle: None,
-                liveness_probe: None,
                 name: "Test Name".to_string(),
-                ports: None,
-                readiness_probe: None,
                 resources: Some(ResourceRequirements {
                     limits: None,
                     requests: Some(resource_requests),
                 }),
-                security_context: None,
-                startup_probe: None,
-                stdin: None,
-                stdin_once: None,
-                termination_message_path: None,
-                termination_message_policy: None,
-                tty: None,
-                volume_devices: None,
-                volume_mounts: None,
-                working_dir: None,
+                ..utils::get_empty_container()
             }],
-            dns_config: None,
-            dns_policy: None,
-            enable_service_links: None,
-            ephemeral_containers: None,
-            host_aliases: None,
-            host_ipc: None,
-            host_network: None,
-            host_pid: None,
-            hostname: None,
-            image_pull_secrets: None,
-            init_containers: None,
-            node_name: None,
             node_selector: Some(node_selector),
-            overhead: None,
-            preemption_policy: None,
-            priority: None,
-            priority_class_name: None,
-            readiness_gates: None,
-            restart_policy: None,
-            runtime_class_name: None,
-            scheduler_name: None,
-            security_context: None,
-            service_account: None,
-            service_account_name: None,
-            share_process_namespace: None,
-            subdomain: None,
-            termination_grace_period_seconds: None,
-            tolerations: None,
-            topology_spread_constraints: None,
-            volumes: None,
+            ..utils::get_empty_pod_spec()
         };
 
         let tagged_result = tag_object(
@@ -286,6 +324,7 @@ mod tests {
             "namespace".to_string(),
             "deployment".to_string(),
             &pod_spec,
+            10,
         );
 
         assert_ne!(tagged_result.len(), 0);
@@ -293,5 +332,38 @@ mod tests {
         assert_eq!(x.image_check, true);
         assert_eq!(x.node_selector_check, true);
         assert_eq!(x.qos_check, true);
+    }
+
+    #[test]
+    fn test_false_cases() {
+        let mut resource_requests = BTreeMap::new();
+        resource_requests.insert("cpu".to_string(), Quantity("400m".to_string()));
+        let pod_spec = PodSpec {
+            containers: vec![Container {
+                image: Some("datadog-agent:7.32.4".to_string()),
+                name: "Test Name".to_string(),
+                resources: Some(ResourceRequirements {
+                    limits: None,
+                    requests: None,
+                }),
+                ..utils::get_empty_container()
+            }],
+            node_selector: None,
+            ..utils::get_empty_pod_spec()
+        };
+
+        let tagged_result = tag_object(
+            "random name".to_string(),
+            "namespace".to_string(),
+            "deployment".to_string(),
+            &pod_spec,
+            10,
+        );
+
+        assert_ne!(tagged_result.len(), 0);
+        let x = tagged_result.first().unwrap();
+        assert_eq!(x.image_check, false);
+        assert_eq!(x.node_selector_check, false);
+        assert_eq!(x.qos_check, false);
     }
 }
